475a5461b6090930cd72b6d1136845c744adf1bc
PearlRut <155261727+PearlRut@users.noreply.github.com>
2026-03-15 21:00:04 +0200

    Update tar_0.lua

--- tar_0.lua
+++ tar_0.lua
@@ -50,7 +50,7 @@ end
     
 
 local function main()
-    OUTPUT_FILE = nil
+    output_file = nil
     -- Get the input folder from command line arguments
     local folder_path = arg[1]
     if folder_path == nil then
